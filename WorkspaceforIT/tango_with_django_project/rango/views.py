7104d47acc6ad50f104be51e9e6ad9e58758b1ce
Yi Hao <2241675H@student.gla.ac.uk>
2017-01-31 19:47:15 +0000

    Chapter-7
    The code in the Chapter 7, without exercise

--- WorkspaceforIT/tango_with_django_project/rango/views.py
+++ WorkspaceforIT/tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 #from django.http import HttpResponse
 
@@ -32,6 +33,22 @@ def show_category(request, category_name_slug):
         context_dict['Page'] = None
     
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+    if form.is_valid():
+        form.save(commit=True)
+        return index(request)
+    else:
+        print (form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
 
 
     
