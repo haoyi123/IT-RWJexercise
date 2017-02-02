8f15c482912ab2fc899f5c84350cba93d87ea4e2
Yi Hao <2241675H@student.gla.ac.uk>
2017-02-02 00:22:24 +0000

    Chapter-8
    All the code in Chapter 8 and the exercise.

--- WorkspaceforIT/tango_with_django_project/rango/views.py
+++ WorkspaceforIT/tango_with_django_project/rango/views.py
@@ -41,11 +41,11 @@ def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
 
-    if form.is_valid():
-        form.save(commit=True)
-        return index(request)
-    else:
-        print (form.errors)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print (form.errors)
 
     return render(request, 'rango/add_category.html', {'form': form})
 
